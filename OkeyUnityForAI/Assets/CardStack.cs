af3bc3a9e71d29a112a8006343c12cdb03796d1a
rc840327 <XDEX@cherri.tech>
2016-06-03 00:33:19 +0800

    1. Add CardStack

---
+++ OkeyUnityForAI/Assets/CardStack.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardStack {
+
+	//Private Variable
+	private card[] cardStack;
+	private int size = 104;
+
+	//Init
+	public CardStack() {
+		this.cardStack = new card[104];
+		for (int i = 0; i < 104; i++) {
+			Color tmp = Color.black;
+			if (i < 26)
+				tmp = Color.black;
+			if (i < 52)
+				tmp = Color.blue;
+			if (i < 78)
+				tmp = Color.red;
+			if (i < 104)
+				tmp = Color.yellow;
+			this.cardStack [i] = new card ((i % 13) + 1, tmp);
+		}
+		this.shuffle ();
+	}
+
+	private void shuffle() {
+		
+		for (int i = 0; i < 500000; i++) {
+			int a = Random.Range(1,10000) % 104;
+			int b = Random.Range(1,10000) % 104;
+			card tmp = cardStack [a];
+			cardStack [a] = cardStack [b];
+			cardStack [b] = tmp;
+		}
+	}
+
+	public card draw() {
+		if (size <= 0) {
+			Debug.LogError ("CardStack Empty");
+		} else {
+			size--;
+			return cardStack[size];
+		}
+		return cardStack[0];
+	}
+}
+

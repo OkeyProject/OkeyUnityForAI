11d133d7e4f861963c149bbb95aedce90ed19b89
Justin <l6104400@gmail.com>
2016-06-02 16:52:20 +0800

    first commit

---
+++ OkeyUnityForAI/Assets/Script/Lib/DECK.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class DECK{
+
+	public static Vector3 CARD_SIZE {get{return new Vector3(0.2f, 0.05f, 0.3f);}}
+
+	public static Vector3[] CARD_POS_0 {
+		get{
+			Vector3[] positionCal = new Vector3[26];
+			for (int i=0; i<13; i++){
+				positionCal[i] = new Vector3(-27 + i*0.45f, -1.25f, -0.08f);
+				positionCal[i+13] = new Vector3(-27 + i*0.45f, -1.92f, -0.08f);
+			}
+			return positionCal;
+		}
+	}
+
+	public static Vector3[] CARD_POS_1 {
+		get{
+			Vector3[] positionCal = new Vector3[26];
+			for (int i=0; i<13; i++){
+				positionCal[i] = new Vector3(4.8f, -1.6f + i*0.45f, -0.08f);
+				positionCal[i+13] = new Vector3(4.15f,-1.6f + i*0.45f, -0.08f);
+			}
+			return positionCal;
+		}
+	}
+
+	public static Vector3[] CARD_POS_2 {
+		get{
+			Vector3[] positionCal = new Vector3[26];
+			for (int i=0; i<13; i++){
+				positionCal[i] = new Vector3(4.8f, 3.8f - i*0.45f, -0.08f);
+				positionCal[i+13] = new Vector3(4.15f, 3.8f - i*0.45f, -0.08f);
+			}
+			return positionCal;
+		}
+	}
+
+	public static Vector3[] CARD_POS_3 {
+		get{
+			Vector3[] positionCal = new Vector3[26];
+			for (int i=0; i<13; i++){
+				positionCal[i] = new Vector3(27 - i*0.45f, 4.08f, -0.08f);
+				positionCal[i+13] = new Vector3(27 - i*0.45f, 3.41f, -0.08f);
+			}
+			return positionCal;
+		}
+	}
+
+	public static Vector3 CARD_ROTATION_POSITIVE_0 {get{return new Vector3(270, 180, 0);}}
+	public static Vector3 CARD_ROTATION_POSITIVE_1 {get{return new Vector3(0, 270, 270);}}
+	public static Vector3 CARD_ROTATION_POSITIVE_2 {get{return new Vector3(0, 90, 90);}}
+	public static Vector3 CARD_ROTATION_POSITIVE_3 {get{return new Vector3(0, 90, 90);}}
+	
+
+}
---
+++ OkeyUnityForAI/Assets/Script/Lib/HAND.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class HAND {
+	public static Vector3 CARD_SCALE {get{return new Vector3(0.25f, 0.05f, 0.375f);}}
+
+	public static Vector3[,] CARD_POS1 {	
+		get{
+			Vector3[,] positionCal = new Vector3[2, 12];
+			for (int i = 0;i < 12;i++){
+				positionCal[0, i] = new Vector3((-2.75f+0.5f*i), -4f, -0.08f);
+				positionCal[1, i] = new Vector3((-2.75f+0.5f*i), -4.75f, -0.08f);
+			}
+			return positionCal;
+		}
+	}
+
+	public static Vector3[,] CARD_POS2 {	
+		get{
+			Vector3[,] positionCal = new Vector3[2, 12];
+			for (int i = 0;i < 12;i++){
+				positionCal[0, i] = new Vector3(4f, (-2.75f+0.5f*i), -0.08f);
+				positionCal[1, i] = new Vector3(4.75f, (-2.75f+0.5f*i), -0.08f);
+			}
+			return positionCal;
+		}
+	}
+
+	public static Vector3[,] CARD_POS3 {	
+		get{
+			Vector3[,] positionCal = new Vector3[2, 12];
+			for (int i = 0;i < 12;i++){
+				positionCal[0, i] = new Vector3((2.75f-0.5f*i), 4f, -0.08f);
+				positionCal[1, i] = new Vector3((2.75f-0.5f*i), 4.75f, -0.08f);
+			}
+			return positionCal;
+		}
+	}
+
+	public static Vector3[,] CARD_POS4 {	
+		get{
+			Vector3[,] positionCal = new Vector3[2, 12];
+			for (int i = 0;i < 12;i++){
+				positionCal[0, i] = new Vector3(-4f, (2.75f-0.5f*i), -0.08f);
+				positionCal[1, i] = new Vector3(-4.75f, (2.75f-0.5f*i), -0.08f);
+			}
+			return positionCal;
+		}
+	}
+
+	public static Vector3 CARD_ROTATION_POSITIVE1 {get{return new Vector3(270, 180, 0);}}
+	public static Vector3 CARD_ROTATION_NEGETIVE1 {get{return new Vector3(270, 0, 0);}}
+
+	public static Vector3 CARD_ROTATION_POSITIVE2 {get{return new Vector3(0, 270, 270);}}
+	public static Vector3 CARD_ROTATION_NEGETIVE2 {get{return new Vector3(0, 270, 90);}}
+
+	public static Vector3 CARD_ROTATION_POSITIVE3 {get{return new Vector3(90, 0, 0);}}
+	public static Vector3 CARD_ROTATION_NEGETIVE3 {get{return new Vector3(90, 180, 0);}}
+
+	public static Vector3 CARD_ROTATION_POSITIVE4 {get{return new Vector3(0, 90, 90);}}
+	public static Vector3 CARD_ROTATION_NEGETIVE4 {get{return new Vector3(0, 90, 270);}}
+
+}
